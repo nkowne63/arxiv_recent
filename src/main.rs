c0a0f6c4fd3914284fbfa0f0ff9b5d398ea702e7
nkowne63rt <neutron63zf@gmail.com>
2023-12-11 16:17:41 +0900

    update: add start_day spec and remove v1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+use core::panic;
+
 use anyhow::Result;
 use arxiv::ArxivQueryBuilder;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDate, Utc};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    start_day: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("version: {}", env!("CARGO_PKG_VERSION"));
-    let dtd = Utc::now().date_naive();
-    let dt = dtd.and_hms_opt(0, 0, 0);
-    let ago = dt.unwrap() - chrono::Duration::days(7);
-    println!("start datetime: {}", ago);
+    let cli = Cli::parse();
+    let start_day = cli
+        .start_day
+        .map(|s| {
+            NaiveDate::parse_from_str(&s, "%y-%m-%d").map(|rn| rn.and_hms_opt(0, 0, 0).unwrap())
+        })
+        .unwrap()
+        .unwrap_or({
+            let dtd = Utc::now().date_naive();
+            let dt = dtd.and_hms_opt(0, 0, 0);
+            dt.unwrap() - chrono::Duration::days(7)
+        });
+    println!("start datetime: {}", start_day);
     let query = ArxivQueryBuilder::new()
         .search_query("cat:cs.PL cat:quant-ph")
         .start(0)
@@ -22,11 +39,15 @@ async fn main() -> Result<()> {
         .filter(|arxiv| {
             let dt = DateTime::parse_from_rfc3339(&arxiv.updated);
             let dt = dt.unwrap().date_naive().and_hms_opt(0, 0, 0).unwrap();
-            dt > ago
+            dt > start_day
         })
         .collect::<Vec<_>>();
     println!("count: {}", arxivs.len());
     for arxiv in arxivs {
+        // remove not "...v1"
+        if !arxiv.id.ends_with("v1") {
+            continue;
+        }
         println!("{} {}", arxiv.id, arxiv.title.replace('\n', ""),);
     }
     Ok(())
